c6f15a2b1a9bb301cd2c87bcf183dcc97ab8a3ef
sdaume <stefan.daume@gmail.com>
2021-03-30 14:59:04 +0200

    Add basic package scaffolding

---
+++ R/twittrcademic.R
@@ -0,0 +1,12 @@
+#' twittrcademic: Collecting Tweet Data Via The Twitter Academic Research
+#' Product Track
+#'
+#' This package provides a collection of utility functions supporting the
+#' collection of Tweet data via Twitter v2 API endpoints in the Twitter Academic
+#' Research product track
+#' \href{https://developer.twitter.com/en/solutions/academic-research}{ Twitter
+#' Academic Research product track}.
+#'
+#' @docType package
+#' @name twittrcademic
+NULL
